8c9a21ad14f5c7d58e7c55616904585e1731d777
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2021-11-03 22:31:50 +0600

    added staked checking at smart contracts

--- src/contracts/GiveFree.sol
+++ src/contracts/GiveFree.sol
@@ -7,6 +7,10 @@ contract GiveFree{
    string public name = "Give Free App";
    DappToken public dapptoken;
    DaiToken public daiToken;
+   address[] public stakers;
+   mapping(address => uint) public stakingBalance;
+   mapping(address => bool) public hasStaked;
+   mapping(address => bool) public isStaking;
 
    constructor(DappToken _daptoken , DaiToken _daiToken) public{
        dapptoken = _daptoken;
@@ -14,5 +18,16 @@ contract GiveFree{
    }
    function stakeTokens(uint _amount) public{
        daiToken.transferFrom(msg.sender, address.this, _amount);
+       stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+       //add user to stakers array only if they haven't staked
+       if(!hasStaked[msg.sender]){
+           stakers.push(msg.sender);
+       }
+       isStaking[msg.sender] = true;
+       hasStaked[msg.sender] = true;
+       
+        
+
    }
+
 }
