6d8507934a30232e18bd76616c2604669ea28e6e
Rahat Saqib <37343956+RahatSaqib@users.noreply.github.com>
2021-11-04 21:13:08 +0600

    added daitOKEN

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Navbar from './Navbar'
 import './App.css'
 import Web3 from 'web3'
+import DaiToken from '../abis/DaiToken.json'
 
 class App extends Component {
 
@@ -13,6 +14,19 @@ class App extends Component {
     const web3 = window.web3
     const account = await web3.eth.getAccounts()
     this.setState({account: account[0]});
+    const networkId = await web3.eth.net.getId();
+    //load daiTOken
+    const daiTokenData = DaiToken.networks[networkId]
+    if(daiTokenData){
+      const daiToken =  new web3.eth.Contract(DaiToken.abi, daiTokenData.address)
+      this.setState({daiToken});
+      let daiTokenBalance =  await daiToken.methods.balanceOf(this.state.account).call()
+      this.setState({daiTokenBalance : daiTokenBalance.toString()})
+    }
+    else{
+      window.alert('DaiTOken contract not deployed')
+    }
+    
   }
   async loadWeb3(){
     if(window.ethereum){
@@ -45,23 +59,6 @@ class App extends Component {
     return (
       <div>
         <Navbar account={this.state.account} />
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                </a>
-
-                <h1>Hello, World!</h1>
-
-              </div>
-            </main>
-          </div>
-        </div>
       </div>
     );
   }
